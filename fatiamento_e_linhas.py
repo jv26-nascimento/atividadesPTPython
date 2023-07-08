c094c9d084a6bf23b58bdd580e437d6c58b5d59a
João Victor Nascimento de Carvalho <jv.26.nascimento@gmail.com>
2023-07-08 13:33:28 -0300

    fatiamento de strings e múltiplas linhas

---
+++ fatiamento_e_linhas.py
@@ -0,0 +1,31 @@
+nome = "João Victor Nascimento de Carvalho"
+
+print(nome[0])
+print(nome[-1])
+print(nome[:4])
+print(nome[5:])
+print(nome[5:11])
+print(nome[5::2])
+print(nome[:])
+print(nome[:: -1])
+
+
+eu = "João"
+
+print(f"""
+Olá, meu nome é {eu}.
+Estou aprendendo Python.
+""")
+
+menu = f"""
+=============== MENU =================
+|                                    |
+|  [1] Depositar                     |
+|  [2] Sacar                         |
+|  [3] Extrato                       |
+|  [4] Cancelar                      |
+|                                    |
+======================================
+"""
+
+print(menu)
