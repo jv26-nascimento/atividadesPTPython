f91e5490f7487ac31a732c09e176f0e4e41ba3ea
João Victor Nascimento de Carvalho <jv.26.nascimento@gmail.com>
2023-07-08 12:43:19 -0300

    exercícios de string

---
+++ interpolacao.py
@@ -0,0 +1,5 @@
+nome = "João"
+idade = 17
+profissao = "estudante"
+
+print(f"Me chamo {nome}, tenho {idade} anos e atualmente sou {profissao}")
---
+++ strings.py
@@ -0,0 +1,21 @@
+nome = "joAozInho"
+
+print(nome.upper())
+print(nome.lower())
+print(nome.title())
+
+print("")
+
+texto = "    olá mundo   "
+
+print(texto + ".")
+print(texto.strip() + ".")
+print(texto.lstrip() + ".")
+print(texto.rstrip() + ".")
+
+print("")
+
+menu = "Python"
+
+print(menu.center(14, "#"))
+print(".".join(menu))
